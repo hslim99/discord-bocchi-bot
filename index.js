9474b4df6bae66744e94747480a79b894091a4e2
Hyunsoo Lim <hslim9909@gmail.com>
2023-01-03 19:56:24 +0900

    feat: delete redundant lines in index.js

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
-
-require('dotenv').config();
-
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({
   intents: [
