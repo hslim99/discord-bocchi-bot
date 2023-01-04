6da0bf14c76f43e13d3131572fdba671490abaa4
Hyunsoo Lim <hslim9909@gmail.com>
2023-01-04 12:23:20 +0900

    fix: dont use connectionstring

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const client = new Client({
 });
 const { Pool } = require('pg');
 exports.pool = new Pool({
-  connectionString: process.env.CONNECTION_STRING,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 const meal = require('./meal');
 const react = require('./react');
